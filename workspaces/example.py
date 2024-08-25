f7b6953e537262dfd30e9ff90fa96331075c2ef3
MasterShifu619 <43004136+MasterShifu619@users.noreply.github.com>
2024-08-25 23:00:29 +0000

    Added python code

---
+++ workspaces/example.py
@@ -0,0 +1,14 @@
+print("Hi. Welcome!")
+def sum(n):
+    sum = 0
+    for i in n:
+        sum += i
+    print(sum)
+def pro(n):
+    pro = 1
+    for i in n:
+        pro =* i
+    print(pro)   
+sum([1,2,3,4,5])
+pro([1,2,3,4,5])
+print("Thank you!")
