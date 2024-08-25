22e9b93365fc09dc7fc0708749fe3561ef4d6850
MasterShifu619 <43004136+MasterShifu619@users.noreply.github.com>
2024-08-25 23:52:03 +0000

    Added test cases

--- workspaces/example.py
+++ workspaces/example.py
@@ -1,14 +1,14 @@
-print("Hi. Welcome!")
+# Below function returns the sum of a given list of numbers
 def sum(n):
     sum = 0
     for i in n:
         sum += i
-    print(sum)
+    return(sum)
+# Below function returns the product of a given list of numbers
 def pro(n):
     pro = 1
     for i in n:
         pro *= i
-    print(pro)   
-sum([1,2,3,4,5])
-pro([1,2,3,4,5])
-print("Thank you!")
+    return(pro)   
+print("The sum of 1, 2, 3, 4 and 5 =",sum([1,2,3,4,5]))
+print("The product of 1, 2, 3, 4 and 5 =",pro([1,2,3,4,5]))
---
+++ workspaces/test1.py
@@ -0,0 +1,4 @@
+from example import sum
+
+def test_should_return_sum():
+    assert sum([1,2,3,4,5]) == 15
