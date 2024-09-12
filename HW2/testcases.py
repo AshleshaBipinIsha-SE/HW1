9cf4fff2cc912336d648833d0d2a69338e441bf5
Ashlesha Joshi <67588914+aj18coep@users.noreply.github.com>
2024-09-12 06:15:37 -0800

    Added second test case

--- HW2/testcases.py
+++ HW2/testcases.py
@@ -4,3 +4,9 @@ from hw2_debugging import merge_sort, recombine
 #Testing for an empty array
 def test_merge_sort_empty_array():
     assert merge_sort([]) == []
+
+#Testing for an array with multiple elements
+def test_merge_sort_manyelements():
+    assert merge_sort([4, 1, 3, 4, 2, 4, 0, 9]) == [0, 1, 2, 3, 4, 4, 4, 9]
+    
+
