5687aa33a11a8766a0b3b160801e8619a77040be
Ashlesha Joshi <67588914+aj18coep@users.noreply.github.com>
2024-09-12 06:22:53 -0800

    Added third test case

--- HW2/testcases.py
+++ HW2/testcases.py
@@ -8,5 +8,9 @@ def test_merge_sort_empty_array():
 #Testing for an array with multiple elements
 def test_merge_sort_manyelements():
     assert merge_sort([4, 1, 3, 4, 2, 4, 0, 9]) == [0, 1, 2, 3, 4, 4, 4, 9]
+
+#Testing for negative elements 
+def test_merge_sort_negatives(): 
+    assert merge_sort([-2, -2, -3, -1, -7, -8]) == [-8, -7, -3, -2, -2, -1]
     
 
