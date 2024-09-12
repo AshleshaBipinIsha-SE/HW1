f6ccb9162c7529f67e760f2ed3c66acf06fe88c0
Ashlesha Joshi <67588914+aj18coep@users.noreply.github.com>
2024-09-12 15:57:42 -0800

    Updated testcases file

--- HW2/testcases.py
+++ HW2/testcases.py
@@ -1,26 +1,18 @@
-import pytest
-from hw2_debugging import merge_sort, recombine
-
-# Testing for an empty array
-
+from hw2_debugging import merge_sort
+"""importing merge sort"""
 
 def test_merge_sort_empty_array():
+    """Testing for an empty array"""
     assert merge_sort([]) == []
 
-# Testing for an array with multiple elements
-
-
 def test_merge_sort_manyelements():
+    """Testing for an array with multiple elements"""
     assert merge_sort([4, 1, 3, 4, 2, 4, 0, 9]) == [0, 1, 2, 3, 4, 4, 4, 9]
 
-# Testing for negative elements
-
-
 def test_merge_sort_negatives():
+    """Testing for negative elements"""
     assert merge_sort([-2, -2, -3, -1, -7, -8]) == [-8, -7, -3, -2, -2, -1]
 
-# Testing for an array with all similar elements
-
-
 def test_merge_sort_similars():
+    """Testing for an array with all similar elements"""
     assert merge_sort([7, 7, 7]) == [7, 7, 7]
